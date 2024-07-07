1db33fe392c48ffe1e05a060264fd6a8bcdf34e4
meghana2367 <maggy.chinta@gmail.com>
2024-07-07 18:14:18 -0500

    downloaded tailwind css

--- djShah/src/components/navbar.jsx
+++ djShah/src/components/navbar.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import {AiOutlineMenu} from 'react-icons/ai';
 
 const Navbar = () => {
     return (
-        <div>Navbar</div>
+        <div>
+            <AiOutlineMenu />
+        </div>
     )
 }
 
